886d3f4ede1dd1bea9ad3a746e3354f01fb1708e
Dmitriy Chebotarev <4ebotarov_dim@inbox.ru>
2025-04-29 23:08:48 +0300

    modified:   lafore1.cpp

---
+++ lafore1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+ int ftemp =0;
+ cout<<"enter tempeaturu po faringeytu: ";
+ cin >> ftemp;
+ int ctemp = (ftemp - 32)*5/9;
+ cout<<"temperatura po celsiu ravna "<< ctemp <<'\n';
+ return 0;
+}
